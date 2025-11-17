7f46adcdcb8bcc757247305ba73c88c17435ffc1
RaphyGamboa <raphael_gamboa@dlsu.edu.ph>
2025-11-17 19:14:48 +0800

    added immunization, patient views

--- views.sql
+++ views.sql
@@ -8,3 +8,29 @@ SELECT
 FROM facility f LEFT JOIN worker hw 		  	  ON f.facilityID = hw.facilityID
 				# TODO: add other joins once medicine inventory and facility link table is created 
 ORDER BY f.FacilityID;
+
+-- PATIENT CONSULTATIONS VIEW (Assigned to RAPHY) --
+CREATE VIEW patientConsultations_view AS
+SELECT 
+    p.patientID,
+    CONCAT(p.lastName, ', ', p.firstName) AS patientName,
+    p.birthDate,
+    p.gender,
+    p.bloodType,
+    p.address,
+    p.primaryPhone,
+    p.emergencyContact,
+    p.patientStatus,
+/* to be added
+    mc.consultationID,
+    mc.consultationDate,
+    mc.diagnosis,
+    mc.symptoms,
+    mc.prescription,
+    mc.status
+*/
+FROM patient p
+/*
+LEFT JOIN medical_consultation mc
+    ON p.patientID = mc.patientID;
+*/
