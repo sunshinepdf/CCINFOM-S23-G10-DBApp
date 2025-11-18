7e51416ed60ebe90a86a84b818fe19874269f053
Khyle Villorente <khyle_raeka_b_villorente@dlsu.edu.ph>
2025-11-18 13:00:59 +0800

    added initial triggers, edited views and records

--- records.sql
+++ records.sql
@@ -1,56 +1,103 @@
-# REFERENCE RECORDS
--- Status Category Table --
-CREATE TABLE REF_StatusCategory (
-    StatusCategoryID INT AUTO_INCREMENT,
-    CategoryName VARCHAR(50) NOT NULL
+-- ============================================================
+-- BARANGAY HEALTH MONITORING SYSTEM DATABASE
+-- Database Schema Script
+-- =====================================================
+DROP SCHEMA IF EXISTS BHMS_DB;
+
+CREATE DATABASE IF NOT EXISTS BHMS_DB;
+USE BHMS_DB;
+
+-- ============================================================
+-- STATUS TABLES (lookup/reference)
+-- ===========================================================
+DROP TABLE IF EXISTS REF_StatusCategory;
+DROP TABLE IF EXISTS REF_Status;
+
+CREATE TABLE IF NOT EXISTS REF_StatusCategory (
+    statusCategoryID INT AUTO_INCREMENT,
+    categoryName VARCHAR(50) NOT NULL,
+    
+    CONSTRAINT status_category_pk PRIMARY KEY (statusCategoryID)
 );
 
--- Status Table --
-CREATE TABLE REF_Status (
-    StatusID INT AUTO_INCREMENT,
-    StatusCategoryID INT NOT NULL,
-    StatusName VARCHAR(50) NOT NULL,
-    FOREIGN KEY (StatusCategoryID) REFERENCES REF_StatusCategory(StatusCategoryID)
+CREATE TABLE IF NOT EXISTS REF_Status (
+    statusID INT AUTO_INCREMENT,
+    statusCategoryID INT NOT NULL,
+    statusName VARCHAR(50) NOT NULL,
+    
+	CONSTRAINT status_reference_pk PRIMARY KEY (statusID),
+    CONSTRAINT status_reference_category_fk FOREIGN KEY (statusCategoryID) REFERENCES REF_StatusCategory(statusCategoryID),
+    CONSTRAINT status_reference_uqname UNIQUE (statusName)
 );
 
-# CORE RECORDS
--- BRGY HEALTH WORKER RECORDS (Assigned to ASHLEY) --
-CREATE TABLE worker (
-	hWorkerID			INT				AUTO_INCREMENT,
-	hWorkerLastName		VARCHAR(45)		NOT NULL,
-	hWorkerFirstName	VARCHAR(45)		NOT NULL,
-	hWorkerPosition		VARCHAR(45)		NOT NULL,
-	hContactInformation	VARCHAR(11)		NOT NULL,
-	hWorkerStatusID INT NOT NULL,
-    FOREIGN KEY (hWorkerStatusID) REFERENCES REF_Status(StatusID),
-    CONSTRAINT worker_pk PRIMARY KEY (hWorkerID),
-    CONSTRAINT workerstatus_fk FOREIGN KEY (hWorkerStatusID) REFERENCES REF_Status(StatusID),
-    CONSTRAINT workerfacility_fk FOREIGN KEY (facilityID) REFERENCES facility(facilityID),
-    CONSTRAINT worker_uqcontact UNIQUE (hContactInformation),
-    CONSTRAINT worker_lastnameCheck CHECK (hWorkerLastName REGEXP '^[A-Za-z]+$'),  
-    CONSTRAINT worker_firstnameCheck CHECK (hWorkerFirstName REGEXP '^[A-Za-z]+$'),
-    CONSTRAINT worker_positionCheck CHECK (hWorkerPosition REGEXP '^[A-Za-z ]+$'),  
-    CONSTRAINT worker_contactCheck CHECK (hContactInformation REGEXP '^[0-9]{11}$')  
-);
+
+-- ============================================================
+-- TABLES: CORE RECORDS
+-- ============================================================
+
+DROP TABLE IF EXISTS medicine;
+DROP TABLE IF EXISTS supplier;
+DROP TABLE IF EXISTS patient;
+DROP TABLE IF EXISTS worker;
+DROP TABLE IF EXISTS facility;
 
 -- BRGY FACILITY RECORDS (Assigned to KHYLE) --
-CREATE TABLE facility (
+CREATE TABLE IF NOT EXISTS facility (
 	facilityID 			INT 			AUTO_INCREMENT,
     facilityName		VARCHAR(60)		NOT NULL,
     facilityAddress 	VARCHAR(100)	NOT NULL, 
-    facilityContactNum	VARCHAR(11)		NOT NULL,
+    facilityContactNum	CHAR(11)		NOT NULL,
     shiftStart			TIME			NOT NULL,
     shiftEnd			TIME			NOT NULL,
     facilityStatusID	INT 			NOT NULL,
     
 CONSTRAINT facility_pk PRIMARY KEY (facilityID),
-CONSTRAINT facilitystatus_fk FOREIGN KEY (facilityStatusID) REFERENCES REF_Status(StatusID),
+CONSTRAINT facilitystatus_fk FOREIGN KEY (facilityStatusID) REFERENCES REF_Status(statusID),
 CONSTRAINT facility_uqname UNIQUE (facilityName), 
-CONSTRAINT facility_contactCheck CHECK (facilityContactNum REGEXP '^[0-9]{11}$'), # CHECKS to ensure only 11 digits from 0-9 are included in the contact number
+CONSTRAINT facility_contactCheck CHECK (facilityContactNum REGEXP '^[0-9]{11}$'),
 CONSTRAINT facility_shiftCheck CHECK (shiftEnd > shiftStart));
 
+-- BRGY HEALTH WORKER RECORDS (Assigned to ASHLEY) --
+CREATE TABLE IF NOT EXISTS worker (
+	hWorkerID			INT				AUTO_INCREMENT,
+	hWorkerLastName		VARCHAR(45)		NOT NULL,
+	hWorkerFirstName	VARCHAR(45)		NOT NULL,
+	hWorkerPosition		VARCHAR(45)		NOT NULL,
+	hContactInformation	CHAR(11)		NOT NULL,
+    hWorkerFacilityID 	INT				NOT NULL,
+	hWorkerStatusID 	INT				NOT NULL,
+    
+    CONSTRAINT worker_pk PRIMARY KEY (hWorkerID),
+    CONSTRAINT workerstatus_fk FOREIGN KEY (hWorkerStatusID) REFERENCES REF_Status(statusID),
+    CONSTRAINT workerfacility_fk FOREIGN KEY (hWorkerFacilityID) REFERENCES facility(facilityID),
+    CONSTRAINT worker_uqcontact UNIQUE (hContactInformation),
+    CONSTRAINT worker_lastnameCheck CHECK (hWorkerLastName REGEXP '^[A-Za-z]+$'),  
+    CONSTRAINT worker_firstnameCheck CHECK (hWorkerFirstName REGEXP '^[A-Za-z]+$'),
+    CONSTRAINT worker_positionCheck CHECK (hWorkerPosition REGEXP '^[A-Za-z ]+$'),  
+    CONSTRAINT worker_contactCheck CHECK (hContactInformation REGEXP '^[0-9]{11}$')
+);
+
+-- PATIENT RECORDS (Assigned to RAPHY) --
+CREATE TABLE IF NOT EXISTS patient (
+	patientID			INT				AUTO_INCREMENT,
+    lastName			VARCHAR(45)		NOT NULL,
+    firstName			VARCHAR(45)		NOT NULL,
+    birthDate			DATE 			NOT NULL,
+    gender				ENUM('Male', 'Female')		NOT NULL,
+    bloodType			ENUM('O+', 'O-', 
+							'A+', 'A-', 
+							'B+', 'B-', 
+							'AB+', 'AB-')			NOT NULL,
+	address				VARCHAR(150)	NOT NULL,
+    primaryPhone		CHAR(11)		NOT NULL,
+    emergencyContact	VARCHAR(80)		NOT NULL,
+    patientStatus		INT				NOT NULL,
+	CONSTRAINT patient_pk PRIMARY KEY (patientID),
+    CONSTRAINT patient_phoneCheck CHECK (primaryPhone REGEXP '^[0-9]{11}$')
+);
+
 -- SUPPLIER RECORDS (Assigned to KHYLE) --
-CREATE TABLE supplier (
+CREATE TABLE IF NOT EXISTS supplier (
 	supplierID 				INT 			AUTO_INCREMENT,
     supplierName 			VARCHAR(50)		NOT NULL,
     supplierAddress 		VARCHAR(100)	NOT NULL,
@@ -64,101 +111,88 @@ CREATE TABLE supplier (
     supplierStatusID     	INT				NOT NULL,
     
     CONSTRAINT supplier_pk PRIMARY KEY (supplierID),
-    CONSTRAINT supplierstatus_fk FOREIGN KEY (supplierStatusID) REFERENCES REF_Status(StatusID),
+    CONSTRAINT supplierstatus_fk FOREIGN KEY (supplierStatusID) REFERENCES REF_Status(statusID),
     CONSTRAINT supplier_name_uq UNIQUE (supplierName),
     CONSTRAINT supplier_contact_chk CHECK (supplierContactNum REGEXP '^[0-9]{11}$'), # CHECKS to ensure only 11 digits from 0-9 are included in the contact number
 	CONSTRAINT supplier_leadtime_chk CHECK (deliveryLeadTime > 0));
 
--- PATIENT RECORDS (Assigned to RAPHY) --
-CREATE TABLE patient (
-	patientID			INT				AUTO_INCREMENT,
-    lastName			VARCHAR(45)		NOT NULL,
-    firstName			VARCHAR(45)		NOT NULL,
-    birthDate			DATE 			NOT NULL,
-    gender				ENUM('Male', 'Female')		NOT NULL,
-    bloodType			ENUM('O+', 'O-', 
-							'A+', 'A-', 
-							'B+', 'B-', 
-							'AB+', 'AB-')			NOT NULL,
-	address				VARCHAR(150)	NOT NULL,
-    primaryPhone		INT				NOT NULL,
-    emergencyContact	VARCHAR(80)		NOT NULL,
-    -- will edit
-    patientStatus		ENUM('dead', 'alive')		NOT NULL,
-    PRIMARY KEY (patientID),
-    CONSTRAINT patient_phoneCheck CHECK (primaryPhone REGEXP '^[0-9]{11}$')
-);
-
--- MEDICINE INVENTORY RECORDS (Assigned to SPENCER) --
-CREATE TABLE medicine_inventory (
-    medID	 		INT 			AUTO_INCREMENT,
-    medName		 	VARCHAR(50) 	NOT NULL UNIQUE,
-    medType	 		ENUM('vaccine', 'medicine') NOT NULL,
-    medDesc		 	VARCHAR(200) 	NOT NULL,
-    quantityInStock INT 			NOT NULL DEFAULT 0,
-    expiryDate 		DATE 			NOT NULL,
-    medStatus	 	ENUM('available', 'expired', 'out of stock') NOT NULL DEFAULT ("available"),
-    PRIMARY KEY (medID),
+-- MEDICINE RECORDS (Assigned to SPENCER) --
+CREATE TABLE IF NOT EXISTS medicine (
+    medicineID	 		INT 			AUTO_INCREMENT,
+    medicineName		VARCHAR(50) 	NOT NULL,
+    medicineDesc		TEXT,
+	dosageForm 			VARCHAR(50),
+    strength 			VARCHAR(50),
+	batchNumber   		VARCHAR(50),
+    medicineStatus 		INT 			NOT NULL,
     
-    CONSTRAINT check_quantity_non_negative CHECK (quantityInStock >= 0),
-    CONSTRAINT check_future_expiry_date CHECK (expiryDate > CURDATE()),
-    CONSTRAINT chk_status_consistency CHECK ((quantityInStock > 0 AND expiryDate > CURDATE() AND medStatus = 'Available') OR
-        (quantityInStock = 0 AND medStatus = 'Out of Stock') OR
-        (expiryDate <= CURDATE() AND medStatus = 'Expired'))
+    CONSTRAINT medicine_pk PRIMARY KEY (medicineID),
+    CONSTRAINT medicinestatus_fk FOREIGN KEY (medicineStatus) REFERENCES REF_Status(statusID),
+    CONSTRAINT medicine_uqname UNIQUE (medicineName),
+    CONSTRAINT medicine_uqbatchnum UNIQUE (batchNumber)
 );
 
-# TRANSACTION RECORDS
--- IMMUNIZATION ADMINISTRATION TRANSACTION RECORDS (Assigned to RAPHY) --
-CREATE TABLE immunization_administration (
-	immunizationID			INT 		AUTO_INCREMENT,
-    patientID 				INT			NOT NULL,
-    medID					INT 		NOT NULL,
-    hWorkerID				INT 		NOT NULL,
-    administrationDate		DATE		NOT NULL,
-    vaccineType				VARCHAR(40)	NOT NULL,
-    batchNumber				INT,
-    dosageNumber 			INT			NOT NULL,
-    nextVaccinationDate 	DATE ,
-    immunizationStatus		ENUM('pending',
-								 'completed')
-										NOT NULL,
-    sideEffects				VARCHAR(100),
-    PRIMARY KEY (immunizationID),
-    FOREIGN KEY (patientID) REFERENCES patient(patientID),
-    FOREIGN KEY (medID) REFERENCES medicine_inventory(medID),
-    FOREIGN KEY (hWorkerID) REFERENCES worker(hWorkerID)
-);
 
+-- ============================================================
+-- LINKING TABLES
+-- ============================================================
+DROP TABLE IF EXISTS medicine_inventory;
+
+-- MEDICINE INVENTORY TABLE  --
+CREATE TABLE IF NOT EXISTS medicine_inventory (
+	inventoryID      INT    AUTO_INCREMENT,
+    facilityID       INT    NOT NULL,
+    medicineID       INT    NOT NULL,
+    quantityInStock  INT    NOT NULL,
+    inventoryStatusID INT   NOT NULL,
+
+	 CONSTRAINT inventory_pk PRIMARY KEY (inventoryID),
+    CONSTRAINT inventory_facility_fk FOREIGN KEY (facilityID) REFERENCES facility(facilityID),
+    CONSTRAINT inventory_medicine_fk FOREIGN KEY (medicineID) REFERENCES medicine(medicineID),
+    CONSTRAINT inventory_status_fk FOREIGN KEY (inventoryStatusID) REFERENCES REF_Status(statusID),
+    CONSTRAINT inventory_qty_chk CHECK (quantityInStock >= 0),
+    CONSTRAINT inventory_facility_medicine_uq UNIQUE (facilityID, medicineID)
+    );
+
+
+-- ============================================================
+-- TRANSACTIONAL RECORDS
+-- ============================================================
+
+DROP TABLE IF EXISTS medical_consultation;
+DROP TABLE IF EXISTS prescription_receipt;
+DROP TABLE IF EXISTS immunization_administration;
+DROP TABLE IF EXISTS restock_invoice;
 
 -- MEDICAL CONSULTATIONS TRANSACTION RECORDS (Assigned to ASHLEY) --
-CREATE TABLE medical_consultation (
-    consultationID INT AUTO_INCREMENT,
-    patientID INT NOT NULL,
-    hWorkerID INT NOT NULL,
-    facilityID INT NOT NULL,
-    consultationDate DATE NOT NULL,
-    consultationTime TIME NOT NULL,
-    symptoms TEXT NOT NULL, 
-    diagnosis TEXT NOT NULL,
-    prescription TEXT,
-    consultationStatusID INT NOT NULL,
+CREATE TABLE IF NOT EXISTS medical_consultation (
+    consultationID 		INT 	AUTO_INCREMENT,
+    patientID 			INT 	NOT NULL,
+    hWorkerID 			INT 	NOT NULL,
+    facilityID 			INT 	NOT NULL,
+    consultationDate 	DATE 	NOT NULL,
+    consultationTime 	TIME 	NOT NULL,
+    symptoms 			TEXT 	NOT NULL, 
+    diagnosis 			TEXT 	NOT NULL,
+    prescription 		TEXT,
+    consultationStatusID INT 	NOT NULL,
     lastUpdated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     
     CONSTRAINT consultation_pk PRIMARY KEY (consultationID),
     CONSTRAINT consultation_patient_fk FOREIGN KEY (patientID) REFERENCES patient(patientID),
     CONSTRAINT consultation_worker_fk FOREIGN KEY (hWorkerID) REFERENCES worker(hWorkerID),
     CONSTRAINT consultation_facility_fk FOREIGN KEY (facilityID) REFERENCES facility(facilityID),
-    CONSTRAINT consultation_status_fk FOREIGN KEY (consultationStatusID) REFERENCES REF_Status(StatusID),
+    CONSTRAINT consultation_status_fk FOREIGN KEY (consultationStatusID) REFERENCES REF_Status(statusID),
     CONSTRAINT consultation_dateCheck CHECK (consultationDate <= CURDATE())
 );
 
 -- PRESCRIPTION RECEIPT TRANSACTION RECORDS (Assigned to KHYLE) --
-CREATE TABLE prescription_receipt (
+CREATE TABLE IF NOT EXISTS prescription_receipt (
 	receiptID			 INT 	 AUTO_INCREMENT,
     patientID			 INT	 NOT NULL,
     consultationID 		 INT	 NOT NULL,
     medicineID			 INT 	 NOT NULL,
-    workerID			 INT	 NOT NULL,
+    hWorkerID			 INT	 NOT NULL,
     distributionDate	 DATE,
     qtyDistributed 		 INT	 NOT NULL,
     isValidPrescription  BOOLEAN DEFAULT FALSE,
@@ -168,24 +202,49 @@ CREATE TABLE prescription_receipt (
     CONSTRAINT prescription_pk PRIMARY KEY (receiptID),
     CONSTRAINT prescription_patient_fk FOREIGN KEY (patientID) REFERENCES patient(patientID),
     CONSTRAINT prescription_medconsult_fk FOREIGN KEY (consultationID) REFERENCES medical_consultation(consultationID),
-    CONSTRAINT prescription_medicine_fk FOREIGN KEY (medicineID) REFERENCES medicine_inventory(medID),
-    CONSTRAINT prescription_healthworker_fk FOREIGN KEY (workerID) REFERENCES worker(hWorkerID),
+    CONSTRAINT prescription_medicine_fk FOREIGN KEY (medicineID) REFERENCES medicine(medicineID),
+    CONSTRAINT prescription_healthworker_fk FOREIGN KEY (hWorkerID) REFERENCES worker(hWorkerID),
+    CONSTRAINT prescription_status_fk FOREIGN KEY (prescriptionStatusID) REFERENCES REF_Status(statusID),
     CONSTRAINT prescreceipt_qty_chk CHECK (qtyDistributed > 0));
     
+    
+-- IMMUNIZATION ADMINISTRATION TRANSACTION RECORDS (Assigned to RAPHY) --
+CREATE TABLE IF NOT EXISTS immunization_administration (
+	immunizationID			INT 		AUTO_INCREMENT,
+    patientID 				INT			NOT NULL,
+    medicineID				INT 		NOT NULL,
+    hWorkerID				INT 		NOT NULL,
+    administrationDate		DATE		NOT NULL,
+    vaccineType				VARCHAR(40)	NOT NULL,
+    dosageNumber 			INT			NOT NULL,
+    nextVaccinationDate 	DATE ,
+    immunizationStatus		INT			NOT NULL,
+    sideEffects				VARCHAR(100),
+    
+    CONSTRAINT immunization_pk PRIMARY KEY (immunizationID),
+    CONSTRAINT immunization_patient_fk FOREIGN KEY (patientID) REFERENCES patient(patientID),
+    CONSTRAINT immunization_medicine_fk FOREIGN KEY (medicineID) REFERENCES medicine(medicineID),
+    CONSTRAINT immunization_worker_fk FOREIGN KEY (hWorkerID) REFERENCES worker(hWorkerID),
+    CONSTRAINT immunization_status_fk FOREIGN KEY (immunizationStatus) REFERENCES REF_Status(statusID));
+
 -- RESTOCK INVOICE TRANSACTION RECORDS (Assigned to SPENCER) --
-CREATE TABLE restock_invoice (
+CREATE TABLE IF NOT EXISTS restock_invoice (
     invoiceID 		INT 			AUTO_INCREMENT,
     supplierID 		INT 			NOT NULL,
-    purchaseOrderID INT 			NOT NULL UNIQUE,
+    purchaseOrderID VARCHAR(16) 	NOT NULL,
     deliveryDate 	DATE 			NOT NULL,
-    receivedBy 		VARCHAR(100) 	NOT NULL,
+    receivedBy 		INT	 			NOT NULL,
     totalOrderCost 	DECIMAL(10,2) 	NOT NULL DEFAULT 0,
-    deliveryStatus 	ENUM('pending', 'ongoing', 'delivered') NOT NULL DEFAULT 'pending',
-    PRIMARY KEY (invoiceID),
-    FOREIGN KEY (supplierID) REFERENCES supplier(supplierID),
+    deliveryStatus 	INT				NOT NULL,
     
+    CONSTRAINT invoice_pk PRIMARY KEY (invoiceID),
+    CONSTRAINT invoice_supplier_fk FOREIGN KEY (supplierID) REFERENCES supplier(supplierID),
+    CONSTRAINT invoice_receivedBy_fk FOREIGN KEY (receivedBy) REFERENCES worker(hWorkerID),
+    CONSTRAINT invoice_status_fk FOREIGN KEY (deliveryStatus) REFERENCES REF_Status(statusID),
+    CONSTRAINT invoice_po_id_uq UNIQUE (purchaseOrderID),
     CONSTRAINT check_valid_delivery_date CHECK (deliveryDate >= CURDATE()),
-    CONSTRAINT check_positive_order_cost CHECK (totalOrderCost >= 0),
-    CONSTRAINT check_positive_po_id CHECK (purchaseOrderID > 0)
+    CONSTRAINT check_positive_order_cost CHECK (totalOrderCost >= 0)
 );
+
+
 
